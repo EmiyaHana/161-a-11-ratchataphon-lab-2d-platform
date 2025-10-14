9ec7b330e0e7d5352065e39e9592663aead98016
EmiyaHana <warm310149@gmail.com>
2025-10-14 11:00:42 +0700

    Add_all_character_script with Inheritance

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    private int health;
+    public int Health
+    { get => health;
+      set => health = (value < 0) ? 0: value; }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"{this.name} take damage {damage}! | Current HP : {Health}");
+    }
+
+    public bool IsDead()
+    {
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log($"{this.name} is dead! Got destroyed!");
+            return true;
+        }
+        else return false;
+    }
+}
